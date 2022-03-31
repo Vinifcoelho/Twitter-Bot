637b40ff2c61c77099ffe18bcbb85a8c1b2ad991
Vinicius Coelho <78565296+Vinifcoelho@users.noreply.github.com>
2022-03-31 16:29:01 -0400

    Update config.py

--- Projects!/config.py
+++ Projects!/config.py
@@ -4,18 +4,18 @@
 
 
 twitter_keys = {
-    'CONSUMER_API_KEY':"R1zYyzsUNSAfE3SKXCTwq2MBZ",
+    'CONSUMER_API_KEY':"--api key goes here--",
 
-    'CONSUMER_API_KEY_SECRET':"6e2yVnArlqVipfHPu5Mv3yihDSX2gmyLfdRoPra15ccweVJh2N",
+    'CONSUMER_API_KEY_SECRET':"--api key secret goes here--",
 
-    'ACCESS_TOKEN':"1502109533900095526-JTUTFAaXanjj4Cxen7VUgIuNXTnMY8",
-    'ACCESS_TOKEN_SECRET':"acec9Y70joVSMCpltOWp5znWri3maqwwjsVd04LvXQQ4a"
+    'ACCESS_TOKEN':"--access token goes here--",
+    'ACCESS_TOKEN_SECRET':"--access token secret goes here--"
 
 }
 
 genius_keys = {
-    "CLIENT_ID":"cFzMC9jdJXLpzyfWqN0jgB1f4ruBXE3RRo95YwcsXGTczOkeQuDzvXZb7YcC3g0z",
-    "CLIENT_ID_SECRET":"irKgYg8SV8gFbR1kxjJet4LCfqimPIg3fsVc2DlCehPis9son4tMjaB9h8LpHOGiRF5X8jVO_eufJHlV4xryTg",
-    "ACCESS_TOKEN":"EQIypCk9GoWHMV1PlGE49g7eLgUA4uxweb87AbsGwxCrRBywrzoW7pw4dJTzy5aw"
+    "CLIENT_ID":"--client id goes here--",
+    "CLIENT_ID_SECRET":"--client id secret goes here--",
+    "ACCESS_TOKEN":"--access token goes here--"
 
 }
